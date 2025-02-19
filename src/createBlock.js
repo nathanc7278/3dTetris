4863de102b1cfb374484644b8cd78a690a7c0e67
sneetchBot <oobleckjetpack@gmail.com>
2025-02-18 16:51:11 -0800

    -

--- src/createBlock.js
+++ src/createBlock.js
@@ -3,18 +3,6 @@ import * as THREE from 'three';
 export function createI(scene) {
     const geometry = new THREE.BoxGeometry(4, 1, 1);
     const material = new THREE.MeshBasicMaterial ({color: 0x0000ff});
-<<<<<<< HEAD
-    let I = new THREE.Mesh( geometry, material );
-    scene.add(I);
-    I.translateY(20.5);
-    I.translateZ(4.5);
-    I.translateX(5);
-    let IBoundingBox = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());  
-    IBoundingBox.setFromObject(I);                   
-    return { I, IBoundingBox };
-} 
-
-=======
     let l = new THREE.Mesh( geometry, material );
     scene.add(l);
     l.translateY(20.5);
@@ -37,4 +25,3 @@ export function createO(scene) {
     lBoundingBox.setFromObject(l);                   
     return { l, lBoundingBox };
 } 
->>>>>>> 992b97260d943cbd029d735e372ab20dc4974792
